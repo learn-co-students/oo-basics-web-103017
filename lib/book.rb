37dd325681ff58b7bc7df7302efc127d618350aa
github username <github email address>
2017-11-01 13:43:53 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,25 @@
+class Book
+  def initialize(title_input)
+    @title = title_input
+  end
 
+  attr_accessor :title
 
+  def author=(author_input)
+    @author = author_input
+  end
+
+  def author
+    @author
+  end
+
+  attr_accessor :page_count
+  attr_accessor :genre
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
+
+# learn spec/01_book_spec.rb # test directory
